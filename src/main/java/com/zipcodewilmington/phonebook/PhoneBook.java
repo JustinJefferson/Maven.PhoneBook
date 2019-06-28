05c8a0b5353c8fda2e2373653983105a64d41248
Justin <justleland@gmail.com>
2019-06-28 15:00:24 -0400

    finished Lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,68 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> map;
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new LinkedHashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> numbers = new LinkedList<>();
+        numbers.add(phoneNumber);
+        map.put(name, numbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> numbers = new LinkedList<>();
+        Collections.addAll(numbers, phoneNumbers);
+        map.put(name, numbers);
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        Boolean entry = map.containsKey(name);
+        if(entry) {
+            return true;
+        }
+        else {
+            List<List<String>> allNumbers = new LinkedList<>(map.values());
+            for(List<String> listNumbers : allNumbers) {
+                if(listNumbers.contains(name)) return true;
+            }
+        }
+
+        return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+        List<String> names = new LinkedList<>(map.keySet());
+        for(String name : names) {
+            if(map.get(name).contains(phoneNumber)) return name;
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        return new LinkedList<>(map.keySet());
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
